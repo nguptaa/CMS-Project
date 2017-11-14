d604577a96aa123aaf1dd0063bfeb17b60a273b3
guptan <nguptaa183@gmail.com>
2017-11-14 07:39:14 +0530

    changes

--- index.php
+++ index.php
@@ -24,10 +24,8 @@
         $post_content=substr($row['post_content'],0,100);
         $post_status=$row['post_status'];
 
-        if($post_status !== 'published'){
-          echo "<h1 class='text-center'>No post sorry</h1>";
-        }
-        else {
+        if($post_status == 'published'){
+
 
           ?>
           <h1 class="page-header">
